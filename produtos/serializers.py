13569337c101a98709bb8a719a00a6b3e78813ad
Wellington Roveder <rovederwellington@gmail.com>
2026-05-11 02:24:41 -0300

    fix: add read_only=True in data_criacao serializer

--- produtos/serializers.py
+++ produtos/serializers.py
@@ -2,7 +2,8 @@ from rest_framework import serializers
 from .models import Produto
 
 class ProdutoSerializer(serializers.ModelSerializer):
-    data_criacao = serializers.DateTimeField(format="%d/%m/%Y %H:%M:%S")
+    data_criacao = serializers.DateTimeField(format="%d/%m/%Y %H:%M:%S", read_only=True)
+    
     class Meta:
         model = Produto
         fields = '__all__'
